3ad24783157dbe6a89eaf808de110a5e17cebacf
darelllien1 <92336339+darelllien1@users.noreply.github.com>
2025-10-06 10:37:53 -0700

    exit screen
    exit nbutton

--- untitledsquirrelgame/Assets/ChampAssets/GameOverScreen.cs
+++ untitledsquirrelgame/Assets/ChampAssets/GameOverScreen.cs
@@ -23,6 +23,6 @@ public class GameOverScreen : MonoBehaviour
 
     public void ExitButton()
     {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("NutCollectorMainMenu");
     }
 }
